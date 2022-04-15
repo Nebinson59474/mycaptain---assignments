cfbe0e68e577ef0f1343c88d068919640b33115d
Nebinson59474 <102983451+Nebinson59474@users.noreply.github.com>
2022-04-15 16:37:50 +0530

    Add files via upload

---
+++ my captain assignment 1.py
@@ -0,0 +1,8 @@
+pi=3.14
+r = float( input("Enter the radius of a circle:"))
+area = pi * r* r
+print("Area of the circle: %.2f" %area)
+
+file_name=input("Enter the file name: ")
+file_extention=file_name.split(".")
+print ("extention of the file is: " + file_extention[-1])
