6689c54814e458e7a214d1faebbf01658f5198b4
Nebinson59474 <102983451+Nebinson59474@users.noreply.github.com>
2022-04-25 13:45:42 +0530

    Add files via upload

--- mycaptain_assignment3.py
+++ mycaptain_assignment3.py
@@ -2,9 +2,12 @@ def most_frequent(string):
     d = dict()
     for key in string:
         if key not in d:
-            d[key] = 1
+            d[key]=1
         else:
-            d[key] += 1
-    return d
+            d[key]+=1
 
-print (most_frequent('mississippi'))
+    return d
+                
+s="mississippi"
+s1=s[-1::-1]
+print (most_frequent(s1))
