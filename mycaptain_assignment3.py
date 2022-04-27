fd0ea10048a892349e88ca42bf8b07e5a3de7476
Nebinson59474 <102983451+Nebinson59474@users.noreply.github.com>
2022-04-27 13:32:39 +0530

    Add files via upload

--- mycaptain_assignment3.py
+++ mycaptain_assignment3.py
@@ -1,13 +1,32 @@
-def most_frequent(string):
-    d = dict()
-    for key in string:
-        if key not in d:
-            d[key]=1
-        else:
-            d[key]+=1
-
-    return d
-                
 s="mississippi"
-s1=s[-1::-1]
-print (most_frequent(s1))
+fre=[None]*len(s)
+char=[]
+for i in s:
+    char.append(i)
+for i in range(0,len(s)):
+    fre[i]=1
+    for j in range ((i+1),len(s)):
+        if char[i]==char[j]:
+            fre[i]=fre[i]+1
+            char[j]="0"
+for i in range(0,len(fre)):
+    for j in range((i+1),len(fre)):
+        temp=0
+        temp1=" "
+        if fre[i]<fre[j]:
+            temp=fre[i]
+            temp1=char[i]
+            fre[i]=fre[j]
+            char[i]=char[j]
+            fre[j]=temp
+            char[j]=temp1
+            
+for i in range(0, len(fre)):
+    if char[i]!=""and char[i]!="0":
+        print(char[i]+"-"+str(fre[i]))
+
+
+
+
+
+        
